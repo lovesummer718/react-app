3eaf0066ebf13ebab03977f6d7547d3599b7e2e8
Wang Xujun <lovesummer718@gmail.com>
2023-07-25 09:05:21 +0800

    add button and alert

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import Alert from "./Alert";
+import Button from "./Button";
 import ListGroup from "./ListGroup";
 
 const handleSelectItem = (item: string) => {
@@ -7,11 +9,13 @@ const handleSelectItem = (item: string) => {
 
 function App() {
   const items = ["Shanghai", "Nanjing", "Hangzhou", "Jinhua", "Lanxi"];
+  const [alertVisible, setAlertVisible] = useState(false);
   return (
     <div>
-      <Alert>
-        Warning <span>Man</span>
-      </Alert>
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisible(false)}>Warning</Alert>
+      )}
+      <Button onClick={() => setAlertVisible(true)}>My Button</Button>
       <ListGroup
         items={items}
         heading={"Cities"}
